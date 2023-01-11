d4619961fa922cc69ef18f024a0964ddce3237f7
LucaArcudi <arcudilu@gmail.com>
2023-01-11 17:39:34 +0100

    add header & main component base strucuture

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,27 @@
+<script>
+
+</script>
+
+<template>
+    <header class="container-fluid text-primary">
+        <div class="row">
+            <div class="col-1">
+                <img src="../assets/img/71-713335_yugioh-logo-yu-gi-oh.png" alt="">
+            </div>
+            <div class="col">
+                <h1>Yu-Gi-Oh Api</h1>
+            </div>
+        </div>
+
+    </header>
+</template>
+
+<style lang="scss" scoped>
+div.row {
+    background-color: aquamarine;
+
+    img {
+        width: 100px;
+    }
+}
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,29 @@
+<script>
+
+</script>
+
+<template>
+    <main>
+        <div class="container-lg">
+            <div class="row">
+                <div class="col-12">
+                    <h1>Content here!</h1>
+                </div>
+            </div>
+        </div>
+    </main>
+</template>
+
+<style lang="scss" scoped>
+main {
+    background-color: aqua;
+
+    div.container-lg {
+        padding: 4rem 7rem;
+
+        div.row {
+            background-color: beige;
+        }
+    }
+}
+</style>
