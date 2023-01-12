5bc81a099c98f6e49470706395b14811792fd615
LucaArcudi <arcudilu@gmail.com>
2023-01-12 20:49:36 +0100

    created CardsComponent component (AppMain child)

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,13 +1,17 @@
 <script>
+import axios from "axios"
+
 import { store } from "../store"
 
-
-
-import axios from "axios"
+import CardsComponent from "./CardsComponent.vue"
 
 export default {
 
     name: "AppMain",
+
+    components: {
+        CardsComponent,
+    },
 
     data() {
         return {
@@ -47,17 +51,7 @@ export default {
     <main>
         <div class="container-lg">
             <div class="row">
-                <h4 class="mt-3">Found {{ store.cardsList.length }} cards</h4>
-                <div v-for="card in store.cardsList" class="card" style="width: calc(100%/5);">
-                    <div class="card-body">
-                        <img class="img-fluid" :src="card.card_images[0].image_url" alt="">
-                        <h5 class="card-title"> {{ card.name }} </h5>
-                        <h6 class="card-subtitle mb-2 text-muted"> {{ card.archetype }} </h6>
-                        <p class="card-text"> {{ card.desc }} </p>
-                    </div>
-                </div>
-
-
+                <CardsComponent />
             </div>
         </div>
     </main>
@@ -72,15 +66,6 @@ main {
 
         div.row {
             background-color: white;
-
-            h4 {
-                color: aqua;
-                font-weight: 700;
-            }
-
-            div.card {
-                background-color: beige;
-            }
         }
     }
 }
---
+++ src/components/CardsComponent.vue
@@ -0,0 +1,34 @@
+<script>
+import { store } from '../store';
+
+export default {
+    data() {
+        return {
+            store,
+        }
+    },
+}
+</script>
+
+<template>
+    <h4 class="mt-3">Found {{ store.cardsList.length }} cards</h4>
+    <div v-for="card in store.cardsList" class="card" style="width: calc(100%/5);">
+        <div class="card-body">
+            <img class="img-fluid" :src="card.card_images[0].image_url" alt="">
+            <h5 class="card-title"> {{ card.name }} </h5>
+            <h6 class="card-subtitle mb-2 text-muted"> {{ card.archetype }} </h6>
+            <p class="card-text"> {{ card.desc }} </p>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+h4 {
+    color: aqua;
+    font-weight: 700;
+}
+
+div.card {
+    background-color: beige;
+}
+</style>
